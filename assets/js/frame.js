98f58bb9257734bac8c38273597ceb7bc71563d9
플랫폼, LMS 구축 전문, AWS TA,AA,SA,DA 지원 <41275565+edumgt@users.noreply.github.com>
2025-01-10 02:09:31 +0000

    2

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -268,23 +268,7 @@ languageSwitcher.addEventListener("click", function (event) {
 
 });
 
-// document.getElementById('closeFloatingNav').addEventListener('click', () => {
-//     const floatingNav = document.getElementById('floatingNav');
-//     floatingNav.classList.add('hidden'); 
-// });
 
-/* gearIcon */
-// const button = document.createElement('button');
-// button.id = 'gearIcon';
-// button.className = 'fixed bottom-4 right-4 bg-gray-800 text-white rounded-full p-3 shadow-lg z-50';
-// const icon = document.createElement('i');
-// icon.className = 'fas fa-globe';
-// button.appendChild(icon);
-// document.getElementById('buttonContainer').appendChild(button);
-// document.getElementById('gearIcon').addEventListener('click', () => {
-//     const floatingNav = document.getElementById('floatingNav');
-//     floatingNav.classList.remove('hidden');
-// });
 
 
 
