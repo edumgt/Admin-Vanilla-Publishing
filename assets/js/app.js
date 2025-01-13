db4c36cd0b3ae96b218dbc5776586dc30fd056ef
플랫폼, LMS 구축 전문, AWS TA,AA,SA,DA 지원 <41275565+edumgt@users.noreply.github.com>
2025-01-13 01:08:31 +0000

    2

--- assets/js/app.js
+++ assets/js/app.js
@@ -399,13 +399,11 @@ if (rows.length > 0) {
 
 
 /* log out */
-
 const memberIcon = document.getElementById('memberIcon');
 const logoutModal = document.getElementById('logoutModal');
 const confirmLogout = document.getElementById('confirmLogout');
 const cancelLogout = document.getElementById('cancelLogout');
 let isLoggedIn = true;
-
 memberIcon.addEventListener('click', function () {
     if (isLoggedIn) {
         logoutModal.classList.remove('hidden');
@@ -413,14 +411,11 @@ memberIcon.addEventListener('click', function () {
         logoutModal.classList.remove('hidden');
     }
 });
-
-
 confirmLogout.addEventListener('click', function () {
     isLoggedIn = false;
     memberIcon.innerHTML = '<i class="fas fa-user"></i>';
     logoutModal.classList.add('hidden');
 });
-
 cancelLogout.addEventListener('click', function () {
     logoutModal.classList.add('hidden');
 });
