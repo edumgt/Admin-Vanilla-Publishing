0323afbc3e7518d4095f5761d004bf357d566e65
kimdypm@gmail.com <kimdypm@gmail.com>
2025-04-02 19:59:56 +0900

    11

--- server.js
+++ server.js
@@ -609,7 +609,7 @@ app.post('/listbox/SiteUser', async (req, res) => {
 const apiList = [
   { "url": "/api/member-permissions", "method": "GET" ,description: '권한 목록'},
   { "url": "/db/inbound", "method": "GET" ,description: 'Local 입고 목록'},
-  { "url": "http://127.0.0.1:8080/api/inbound", "method": "GET" ,description: 'Boot 입고 목록, 토큰 필요'},
+  { "url": "http://127.0.0.1:8080/api/inbound", "method": "GET" ,description: 'Spring Boot 입고 목록'},
   { "url": "/db/outbound", "method": "GET" ,description: '출고 목록'},
 
   
